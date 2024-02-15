df6ea9ea06912af376863068b5c2bcef0bcdd302
nngig <ngigi.francis5@gmail.com>
2024-02-15 08:15:34 +0300

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,30 @@
-import React from "react";
-import { actors } from "../data";
+import { useEffect, useState } from "react";
+import Card from "../components/ReusableCard";
+import NavBar from "../components/NavBar";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const [actors, setActors] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/actors")
+    .then(r => r.json())
+    .then(data => setActors(data))
+    .catch(error => console.error(error))
+  }, [])
+
+  const actorList = actors.map(actor => <Card key={actor.id} name={actor.name} movies={actor.movies} />)
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Actors Page</h1>
+        {actorList}
+      </main>
+    </>
+  );
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,30 @@
-import React from "react";
-import { directors } from "../data";
+import { useEffect, useState } from "react";
+import Card from "../components/ReusableCard";
+import NavBar from "../components/NavBar";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const [directors, setDirectors] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/directors")
+    .then(r => r.json())
+    .then(data => setDirectors(data))
+    .catch(error => console.error(error))
+  }, [])
+
+  const directorList = directors.map(director => <Card key={director.id} name={director.name} movies={director.movies} />)
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Directors Page</h1>
+        {directorList}
+      </main>
+    </>
+  );
 }
 
 export default Directors;
